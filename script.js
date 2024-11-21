e298fd65ed370352cbc5a9353732ed11be630c8e
Arifk-commit <khanarifshamshul@gmail.com>
2024-11-21 16:39:45 +0530

    modified

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
-let arr = document.querySelectorAll(".appoint"); 
-let conatiner_o_dent = document.querySelector(".container_o_dent")
+// ** animation in the appointment part  **//
 
-// dentist appointment
+// seprate code for each appointment 
 
-let dent_over = document.querySelector("#dent_over ")
- dent_over.addEventListener("mouseenter",() =>{
-    dent_over.classList.add("app")
-   
- })
- dent_over.addEventListener("mouseleave",() =>{
-    dent_over.classList.remove("app")
-   
- })
- dent_over.addEventListener("click",()=>{
-  const newlink1 =  document.createElement('a')
-   newlink1.href = "https://chatgpt.com";
-   
-   conatiner_o_dent.appendChild(newlink1);
- })
+// let appoints = document.querySelectorAll(".appoint"); 
 
-//  dietitian appointment
+// // dentist appointment
 
-let diet_over = document.querySelector("#diet_over ")
- diet_over.addEventListener("mouseenter",() =>{
-    diet_over.classList.add("app")
+// let dent_over = document.querySelector("#dent_over ")
+//  dent_over.addEventListener("mouseenter",() =>{
+//     dent_over.classList.add("cl")
    
- })
- diet_over.addEventListener("mouseleave",() =>{
-    diet_over.classList.remove("app")
+//  })
+//  dent_over.addEventListener("mouseleave",() =>{
+//     dent_over.classList.remove("cl")
    
- })
+//  })
+ 
+// //  dietitian appointment
 
-//  surgeon appointment
-
-let surg_over = document.querySelector("#surg_over ")
- surg_over.addEventListener("mouseenter",() =>{
-    surg_over.classList.add("app")
+// let diet_over = document.querySelector("#diet_over ")
+//  diet_over.addEventListener("mouseenter",() =>{
+//     diet_over.classList.add("cl")
    
- })
- surg_over.addEventListener("mouseleave",() =>{
-    surg_over.classList.remove("app")
+//  })
+//  diet_over.addEventListener("mouseleave",() =>{
+//     diet_over.classList.remove("cl")
    
- })
+//  })
 
-// for (const el of arr) {
-//     el.addEventListener("mouseover" ,()=>{
-        
-//     })
-// }
+// //  surgeon appointment
+
+// let surg_over = document.querySelector("#surg_over ")
+//  surg_over.addEventListener("mouseenter",() =>{
+//     surg_over.classList.add("cl")
+   
+//  })
+//  surg_over.addEventListener("mouseleave",() =>{
+//     surg_over.classList.remove("cl")
+   
+//  })
+
+
+// using for loop //
+
+let appoints = document.querySelectorAll(".appointment")
+for (const appoint of appoints) {
+    appoint.addEventListener("mouseenter" ,()=>{
+        appoint.classList.add("cl");
+    })
+    appoint.addEventListener("mouseleave" ,()=>{
+        appoint.classList.remove("cl");
+    })
+    
+}
+
+
+// Animation for footer part 
+
+let links = document.querySelectorAll(".link");
+for (const link of links) {
+   link.addEventListener("mouseenter",()=>{
+      link.classList.add("cl2")
+      // link.style.textDecoration = 'underline';
+   
+   });
+   link.addEventListener("mouseleave", ()=>{
+      link.classList.remove("cl2")
+   })
+   
+}
+let links2 = document.querySelectorAll(".link2");
+for (const link2 of links2) {
+   link2.addEventListener("mouseenter",()=>{
+      link2.classList.add("cl2")
+      // link.style.textDecoration = 'underline';
+   
+   });
+   link2.addEventListener("mouseleave", ()=>{
+      link2.classList.remove("cl2")
+   })
+   
+}
